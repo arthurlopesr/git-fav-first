7515fb2012631d9237df9ac58070c3f5040d6e3a
Arthur Lopes <arthurlopr12@gmail.com>
2023-06-11 14:52:51 -0300

    Favorites

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,23 +1,109 @@
+export class GithubUsers {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+        return fetch(endpoint)
+            .then(data => data.json())
+            .then(({ login, name, public_repos, followers }) => ({
+                login,
+                name,
+                public_repos,
+                followers,
+            }))
+    }
+}
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+
+        GithubUsers.search('arthurlopesr').then(user => console.log(user))
+    }
+
+    load() {
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    async add(username) {
+        const user = await GithubUsers.search(username)
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+        this.update()
     }
 }
 
 export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
-
+        this.tbody = this.root.querySelector('tbody')
         this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
         this.removeAllTr()
+        this.entries.forEach(user => {
+            const row = this.creatRow();
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+                if (isOk) {
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+        })
+
+    }
+
+    creatRow() {
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+        <td class="user">
+            <img src="https://github.com/arthurlopesr.png" alt="Imagem Arthur">
+            <a href="https://github.com/arthurlopesr" target="_blank">
+                <p>Arthur Lopes</p>
+                <span>arthurlopesr</span>
+            </a>
+        </td>
+        <td class="repositories">
+            80
+        </td>
+        <td class="followers">
+            9999
+        </td>
+        <td>
+            <button class="remove">&times;</button>
+        </td>
+        `
+
+        return tr
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector('tbody')
-        tbody.querySelectorAll('tr').forEach(tr => {
+        this.tbody.querySelectorAll('tr').forEach(tr => {
             tr.remove()
         });
     }
